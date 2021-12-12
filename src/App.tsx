a0d75f17fb92b77acb676423a5763b1dc4ee9aa1
Ketan Agrawal <agrawalk@stanford.edu>
2021-12-12 03:25:02 -0800

    stylized app further

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import axios from 'axios';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import {ReactComponent as PlusCircle} from './plus-circle.svg';
+import {ReactComponent as TrashCan} from './trash-2.svg';
+import {ReactComponent as ThumbsUp} from './thumbs-up.svg';
+import {ReactComponent as ThumbsDown} from './thumbs-down.svg';
+
 
 interface TrackInfo {
     id: string;
     name: string;
     artists: string[];
-    albumArtURL: string;
+    /* albumArtURL: string; */
     previewURL: string;
     //TODO: audio preview??
 }
@@ -23,79 +28,52 @@ interface RecommendedTrackInfo extends TrackInfo  {
     onDislike: React.MouseEventHandler<HTMLElement>;
 }
 
-function SearchTrack({ id, name, artists, previewURL, albumArtURL, onAdd }: SearchTrackInfo) {
+function BasicTrack({ id, name, artists }: TrackInfo) {
     return (
-        <div className="border-2 border-gray-500 rounded flex justify-between">
-            <div className="flex">
-                <img
-                    src={albumArtURL}
-                    alt={`${name} - ${artists.join(', ')} album art`}
-                    width="64"
-                    height="64"
-                />
-                <div className="flex flex-col">
-                    <p>
-                        {name} - {artists.join(', ')}
-                    </p>
-                    <iframe title={id} src={`https://open.spotify.com/embed/track/${id}`} width="300" height="80" frameBorder="0" allowTransparency={true} allow="encrypted-media" />
-                    {/* <audio
-                        controls
-                        src={previewURL}>
-                        Your browser does not support the
-                        <code>audio</code> element.
-                        </audio> */}
-                </div>
-            </div>
-            <button className="text-green-400" onClick={onAdd}>+</button>
-        </div>
-    );
-}
-function PlaylistTrack({ id, name, artists, previewURL, albumArtURL, onDelete }: PlaylistTrackInfo) {
-    return (
-        <div className="border-2 border-gray-500 rounded flex justify-between">
-            <div className="flex">
-            <img
-                src={albumArtURL}
-                alt={`${name} - ${artists.join(', ')} album art`}
-                width="64"
-                height="64"
-            />
-            <div className="flex flex-col">
-                <p>
-                    {name} - {artists.join(', ')}
-                </p>
-                <iframe title={id} src={`https://open.spotify.com/embed/track/${id}`} width="300" height="80" frameBorder="0" allowTransparency={true} allow="encrypted-media" />
-                {/* <audio
-                    controls
-                    src={previewURL}>
-                    Your browser does not support the
-                    <code>audio</code> element.
-                    </audio> */}
-            </div>
-            </div>
-            <button className="text-red-400" onClick={onDelete}>Delete</button>
+        <div className="flex flex-col">
+            {/* <p className="truncate text-2xl font-bold">{name}</p>
+                <p>{artists.join(', ')}</p> */}
+            <iframe title={id} src={`https://open.spotify.com/embed/track/${id}`} width="300" height="80" frameBorder="0" allow="encrypted-media" />
+            {/* <audio
+                controls
+                src={previewURL}>
+                Your browser does not support the
+                <code>audio</code> element.
+                </audio> */}
         </div>
     );
 }
 
-function RecommendedTrack({ id, name, artists, albumArtURL, onLike, onDislike }: RecommendedTrackInfo) {
-    console.log('art URL: ' + albumArtURL)
+function SearchTrack({ onAdd, ...trackInfo }: SearchTrackInfo) {
     return (
-        <div className="border-2 border-gray-500 rounded flex flex-col items-center">
-            {/* <img
-                src={albumArtURL}
-                alt={`${name} - ${artists.join(', ')} album art`}
-                width="200"
-                height="200"
-                /> */}
-            <iframe title={id} src={`https://open.spotify.com/embed/track/${id}`} width="300" height="80" frameBorder="0" allowTransparency={true} allow="encrypted-media" />
-            <p>
-                {name} - {artists.join(', ')}
-            </p>
-            <p><b>Is this a good addition to your playlist?</b></p>
-            <div className="flex space-x-4">
-                <button className="border border-gray-300 rounded" onClick={onDislike}>👎</button>
-                <button className="border border-gray-300 rounded" onClick={onLike}>👍</button>
+        <div className="border-2 border-gray-500 rounded flex justify-between p-2 space-x-2">
+            <BasicTrack { ...trackInfo } />
+            <button onClick={onAdd}><PlusCircle className="stroke-current hover:text-green-500" /></button>
+        </div>
+    );
+}
+
+function PlaylistTrack({ onDelete, ...trackInfo }: PlaylistTrackInfo) {
+    return (
+        <div className="border-2 border-gray-500 rounded flex justify-between p-2 space-x-2">
+            <BasicTrack { ...trackInfo }/>
+            <button onClick={onDelete}><TrashCan className="stroke-current hover:text-red-500" /></button>
+        </div>
+    );
+}
+
+function RecommendedTrack({ onLike, onDislike, ...trackInfo }: RecommendedTrackInfo) {
+    return (
+        <div className="border-2 border-gray-500 rounded flex flex-col items-center p-2">
+            <BasicTrack { ...trackInfo }/>
+            <p className="text-center"><b>Is this a good addition to your playlist?</b></p>
+            <div className="flex space-x-4 justify-center">
+                <button className="border border-gray-300 rounded" onClick={onDislike}>
+                    <ThumbsDown className="stroke-current hover:text-blue-500"/>
+                </button>
+                <button className="border border-gray-300 rounded" onClick={onLike}>
+                    <ThumbsUp className="stroke-current hover:text-blue-500"/>
+                </button>
             </div>
         </div>
     );
@@ -105,32 +83,45 @@ interface PlaylistCreationInfo {
     playlistName: string;
     playlistDescription: string;
     onNameChange: React.ChangeEventHandler<HTMLInputElement>;
-    onDescriptionChange: React.ChangeEventHandler<HTMLInputElement>;
+    onDescriptionChange: React.ChangeEventHandler<HTMLTextAreaElement>;
     onSubmit: React.FormEventHandler<HTMLFormElement>;
 }
 
 function PlaylistCreation({playlistName, playlistDescription, onNameChange,
                            onDescriptionChange, onSubmit}: PlaylistCreationInfo) {
+    const formIncomplete = playlistName.length === 0 || playlistDescription.length === 0
+    const enabledStyles = formIncomplete ? "opacity-50" : "hover:border-blue-500 hover:text-blue-500 cursor-pointer"
     return (
-        <form onSubmit={onSubmit}>
-            <p>Playlist Name:</p>
-            <input
-                className="border-2 border-gray-300 rounded"
-                type="text"
-                placeholder="Give your playlist a name."
-                value={playlistName}
-                onChange={onNameChange}
-            />
-            <p>Playlist Description:</p>
-            <input
-                className="border-2 border-gray-300 rounded"
-                type="text"
-                placeholder="Give a brief description of what your aims for this playlist are."
-                value={playlistDescription}
-                onChange={onDescriptionChange}
-            />
-            <input type="submit" value="Submit" />
-        </form>
+        <div className="flex flex-col items-center">
+            <form onSubmit={onSubmit} className="w-96 space-y-4">
+                <div>
+                    <p>Playlist Name:</p>
+                    <input
+                        className="border-2 border-gray-300 rounded"
+                        type="text"
+                        placeholder="Give your playlist a name"
+                        value={playlistName}
+                        onChange={onNameChange}
+                    />
+                </div>
+                <div>
+                    <p>Playlist Description:</p>
+                    <textarea
+                        name="playlist-description" id="playlist-description" rows={3} cols={50}
+                        className="border-2 border-gray-300 rounded"
+                        placeholder="Give a brief description of what your aims for this playlist are."
+                        value={playlistDescription}
+                        onChange={onDescriptionChange}
+                    />
+                </div>
+                <input
+                    className={`py-2 px-4 border-2 border-black bg-white font-semibold rounded-lg ${enabledStyles}`}
+                    type="submit"
+                    value="Create"
+                    disabled={formIncomplete}
+                />
+            </form>
+        </div>
     )
 }
 
@@ -144,40 +135,52 @@ function App() {
     const [searchResults, setSearchResults] = useState<TrackInfo[]>([]);
     const [recs, setRecs] = useState<TrackInfo[]>([]);
     const [displayedRec, setDisplayedRec] = useState(0);
+    useEffect(() => {
+        console.log(searchResults);
+    }, [searchResults])
+
+    useEffect(() => {
+        console.log(tracks);
+    }, [tracks])
 
     const handlePlaylistCreation: React.FormEventHandler<HTMLFormElement> = (e) => {
         e.preventDefault();
         setPlaylistCreated(true);
     }
 
+    //TODO: refactor
     if (!playlistCreated) {
         return (
-            <PlaylistCreation
-                playlistName={playlistName}
-                onNameChange={e => setPlaylistName(e.target.value)}
-                playlistDescription={playlistDescription}
-                onDescriptionChange={e => setPlaylistDescription(e.target.value)}
-                onSubmit={handlePlaylistCreation}
-            />
+            <div className="App m-4">
+                <h1 className="text-2xl font-semibold border-b mb-4">Playlist Recommender</h1>
+                <PlaylistCreation
+                    playlistName={playlistName}
+                    onNameChange={e => setPlaylistName(e.target.value)}
+                    playlistDescription={playlistDescription}
+                    onDescriptionChange={e => setPlaylistDescription(e.target.value)}
+                    onSubmit={handlePlaylistCreation}
+                />
+            </div>
         )
     }
-    //TODO: refactor this into a component
-    const dummyTracks: TrackInfo[] = [
-        {
-            id: "1",
-            name: 'Redbone',
-            artists: ['Childish Gambino'],
-            albumArtURL: 'https://i.scdn.co/image/ab67616d00004851b08b996d08001270adc8b555',
-            previewURL: 'too lazy for this rn',
-        },
-        {
-            id: "2",
-            name: 'IV. Sweatpants',
-            artists: ['Childish Gambino'],
-            albumArtURL: 'https://i.scdn.co/image/ab67616d00004851fce23dadb51975ebf2e9d75c',
-            previewURL: 'too lazy for this rn',
-        },
-    ]
+
+    // TODO: refactor this into a component
+    // const dummyTracks: TrackInfo[] = [
+    //     {
+    //         id: "1",
+    //         name: 'Redbone',
+    //         artists: ['Childish Gambino'],
+    //         /* albumArtURL: 'https://i.scdn.co/image/ab67616d00004851b08b996d08001270adc8b555', */
+    //         previewURL: 'too lazy for this rn',
+    //     },
+    //     {
+    //         id: "2",
+    //         name: 'IV. Sweatpants',
+    //         artists: ['Childish Gambino'],
+    //         /* albumArtURL: 'https://i.scdn.co/image/ab67616d00004851fce23dadb51975ebf2e9d75c', */
+    //         previewURL: 'too lazy for this rn',
+    //     },
+    // ]
 
     const fetchSearchResults: React.FormEventHandler<HTMLFormElement> = async (e) => {
         e.preventDefault();
@@ -193,9 +196,11 @@ function App() {
             console.error(error);
         }
     }
-    const addToPlaylist = (track: TrackInfo) => {
-        // TODO: duplicate detection
-        setTracks([...tracks, track])
+
+    const addToPlaylist = (track: TrackInfo) => () => {
+        if (!tracks.includes(track)) {
+            setTracks([...tracks, track])
+        }
     }
     const deleteTrack = (id: string) => () => {
         setTracks(tracks.filter(track => id !== track.id))
@@ -203,7 +208,6 @@ function App() {
     const fetchRecommendations = async () => {
         //TODO: call [X recommendation API] on the backend
         //TODO: call get_track(id) to get extra track info
-        console.log('this happening')
         const response = await axios.get('/recs', {
             params: {
                 track_ids: tracks.map(t => t.id)
@@ -211,21 +215,29 @@ function App() {
         })
         setRecs(response.data.tracks);
     }
-    const dummyAdvanceRecs = () => {
-        //TODO: parametrize w/ like/dislike
+
+    const storeRecommendationResponse = (likedCurrentTrack: boolean) => {
+
+    }
+
+    const dummyAdvanceRecs = (likedCurrentTrack: boolean) => () => {
         //TODO: store like/dislike data in database
-        //TODO: advance to some sort of endgame state
+        storeRecommendationResponse(likedCurrentTrack);
+        //TODO: advance to some sort of endgame state after last rec track
         if (displayedRec < recs.length - 1) {
             setDisplayedRec(displayedRec + 1);
         }
     }
 
+    const searchIncomplete = searchText.length === 0;
+    const enabledStyles = searchIncomplete ? "opacity-50" : "hover:border-blue-500 hover:text-blue-500 cursor-pointer"
+
     return (
-        <div className="App">
-            <h1 className="text-xl border-b">Playlist Recommender</h1>
-            <div className="flex justify-around">
-                <div>
-                    <form onSubmit={fetchSearchResults}>
+        <div className="App m-4">
+            <h1 className="text-2xl font-semibold border-b mb-4">Playlist Recommender</h1>
+            <div className="flex justify-center space-x-16">
+                <div className="w-96 space-y-4 max-h-screen">
+                    <form className="space-x-2" onSubmit={fetchSearchResults}>
                         <input
                             className="border-2 border-gray-300 rounded"
                             type="text"
@@ -233,23 +245,34 @@ function App() {
                             value={searchText}
                             onChange={e => setSearchText(e.target.value)}
                         />
-                        <input type="submit" value="Submit" />
+                        <input
+                            className={`px-2 border-2 border-black bg-white font-semibold rounded-lg ${enabledStyles}`}
+                            type="submit"
+                            value="Search"
+                            disabled={searchIncomplete}
+                        />
                     </form>
-            {searchResults.map(track => <SearchTrack {...track} key={track.id} onAdd={() => addToPlaylist(track)} />)}
+                    <div className="space-y-4 max-h-full overflow-scroll">
+                        {searchResults.map(track => <SearchTrack {...track} key={track.id} onAdd={addToPlaylist(track)} />)}
+                    </div>
                 </div>
-                <div>
-                    <p>Your Playlist <b>{playlistName}</b>:</p>
-                    <p><i>{playlistDescription}</i></p>
-                    {tracks.map(track => <PlaylistTrack {...track} key={track.id} onDelete={deleteTrack(track.id)} />)}
-                    <button onClick={fetchRecommendations}>Get Recommendations</button>
+                <div className="w-96 space-y-4 max-h-screen">
+                    <div>
+                        <p>Your Playlist <b>{playlistName}</b>:</p>
+                        <p><i>{playlistDescription}</i></p>
+                    </div>
+                    <div className="space-y-4 max-h-full overflow-scroll">
+                        {tracks.map(track => <PlaylistTrack {...track} key={track.id} onDelete={deleteTrack(track.id)} />)}
+                    </div>
+                    <button className="border border-gray-300 rounded" onClick={fetchRecommendations}>Get Recommendations</button>
                     {recs.length > 0 && (
                         <>
                             <p>Recommended Tracks:</p>
                             <RecommendedTrack
                                 {...recs[displayedRec]}
                                 key={recs[displayedRec].id}
-                                onLike={dummyAdvanceRecs}
-                                onDislike={dummyAdvanceRecs}
+                                onLike={dummyAdvanceRecs(true)}
+                                onDislike={dummyAdvanceRecs(false)}
                             />
                         </>
                     )}
